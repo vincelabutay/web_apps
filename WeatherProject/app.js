e15bfa70cc133c5f1ccfa6de6fe515cb2ea2bacf
vincelabutay <vincelabutay@gmail.com>
2024-08-01 19:04:16 +0800

    done weatherapp

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,24 +1,48 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html")
+});
+
+app.post("/", function(req, res){
+  
+  const query = req.body.cityName;
+  const apiKey = "41632e65957528ef70adc51819cd28ad";
+  const unit = "metric";
   const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=london&appid=41632e65957528ef70adc51819cd28ad&units=metric";
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&appid=" +
+    apiKey +
+    "&units=" + unit;
   https.get(url, function (response) {
     console.log(response.statusCode);
 
     response.on("data", function (data) {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.main.temp;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
       //   console.log(weatherData);
-      console.log(temp);
+      // console.log(temp);
+      res.write("<p>The weather is currently " + weatherDescription + "</p>");
+      res.write(
+        "<h1>The temperature in " + query + " is " + temp + " degrees Celcius.</h1>"
+      );
+      res.write("<img src=" + imageURL + ">");
+      res.send();
     });
   });
+})
 
-  res.send("Server is up and running");
-});
+
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
